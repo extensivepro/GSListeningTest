c6790c8faadf431911d28cea7e78fb23dbd31615
guanbo <guanbo2002@gmail.com>
2012-10-10 12:57:40 +0800

    防止界面不响应，限制到只遍历可视范围的记录

--- UITest/Listening/UIAListeningRecord.js
+++ UITest/Listening/UIAListeningRecord.js
@@ -1,8 +1,9 @@
 #import "UIAListeningResult.js"
 
-var traverseCells = function(target, cells) {
-	for(var i = 0, len = cells.length; i < len; i++ ) {
-		cells[i].tap()
+var traverseCells = function(target, tableView) {
+	for(var i = 0, len = tableView.cells().length; i < len; i++ ) {
+		if(tableView.cells()[i].isVisible() === 0) break;
+		tableView.cells()[i].tap()
 		UIAListeningResult.show(target)
 	}		
 }
@@ -13,7 +14,7 @@ var UIAListeningRecord = {
 		UIALogger.logMessage("刷新记录")
 		target.frontMostApp().mainWindow().tableViews()[0].dragInsideWithOptions({startOffset:{x:0.64, y:0.56}, endOffset:{x:0.71, y:0.91}})
 		
-		traverseCells(target, target.frontMostApp().mainWindow().tableViews()[0].cells())
+		traverseCells(target, target.frontMostApp().mainWindow().tableViews()[0])
 		
 		target.frontMostApp().navigationBar().leftButton().tap();
 		UIALogger.logPass("听写记录列表")
@@ -22,7 +23,7 @@ var UIAListeningRecord = {
 		UIALogger.logStart("听力错误重做列表")
 		UIALogger.logMessage("刷新记录")
 		target.frontMostApp().mainWindow().tableViews()[0].dragInsideWithOptions({startOffset:{x:0.64, y:0.56}, endOffset:{x:0.71, y:0.91}})
-		traverseCells(target, target.frontMostApp().mainWindow().tableViews()[0].cells())
+		traverseCells(target, target.frontMostApp().mainWindow().tableViews()[0])
 		target.frontMostApp().navigationBar().leftButton().tap();
 		UIALogger.logPass("听力错误重做列表")
 	}
