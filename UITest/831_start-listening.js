0c6725e58feda6f6e0d72e728d045b5f49f1b6b6
chenzhenuaa <chenyanlyh@gmail.com>
2012-10-11 13:59:36 +0800

    add test js/start listen

---
+++ UITest/831_start-listening.js
@@ -0,0 +1,30 @@
+#import "User/UIAUserCenter.js"
+
+UIALogger.logStart("CASE #831::开始听力考试");
+var target = UIATarget.localTarget();
+
+UIAUserCenter.relogin();
+
+//UIATarget.localTarget().pushTimeout(5);
+//target.frontMostApp().mainWindow().buttons()["登录"].tap();
+/*UIATarget.localTarget().popTimeout();
+target.frontMostApp().mainWindow().textFields()["Username"].tap();
+target.frontMostApp().keyboard().typeString("1");
+target.frontMostApp().keyboard().typeString("23@\n");
+target.frontMostApp().keyboard().typeString("1");
+target.frontMostApp().keyboard().typeString("23456\n");
+*/
+target.frontMostApp().tabBar().buttons()["好友"].tap();
+
+UIALogger.logMessage("进入听力考试概况页面");
+target.frontMostApp().tabBar().buttons()["好友"].tap();
+target.frontMostApp().mainWindow().tableViews()[0].cells()[0].tap();
+target.frontMostApp().mainWindow().tableViews()[0].cells()[1].tap();
+
+target.delay(3);
+
+target.frontMostApp().mainWindow().tableViews()[0].cells()[1].tap();
+
+target.frontMostApp().mainWindow().tableViews()[0].cells().firstWithPredicate("name beginswith '开始考试'").tap();
+
+UIALogger.logPass("CASE #831::开始听力考试");
