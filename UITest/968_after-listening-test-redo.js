3db5fe47eef6dc19821a7461f9f7bab3c996a5ba
chenzhenuaa <chenzhenuaa@gmail.com>
2012-10-18 17:33:35 +0800

    alter test js case #968:考试完成后错题重做

--- UITest/968_after-listening-test-redo.js
+++ UITest/968_after-listening-test-redo.js
@@ -1,34 +1,63 @@
-#import "User/UIAUserCenter.js"
-
 UIALogger.logStart("CASE #968::考试完成后错题重做");
 
 var target=UIATarget.localTarget();
-UIAUserCenter.relogin();
+
+UIATarget.onAlert = function onAlert(alert) {
+	var title = alert.name();
+	
+    // test if your script should handle the alert, and if so, return true
+	var info = alert.staticTexts()[1];
+	UIALogger.logDebug(info.name());
+	if(info.name() == "没有错题") {
+		alert.defaultButton().tap();
+        UIALogger.logDebug("错题数为零验证");
+		return true;
+	} 
+	
+	// add a warning to the log for each alert encountered
+    UIALogger.logWarning("Alert with title '" + title + "' encountered!");
+    
+    // otherwise, return false to use the default handler
+    return false;
+}
+
+target.frontMostApp().tabBar().buttons()["我"].tap();
+if(target.frontMostApp().navigationBar().buttons()["注销"].checkIsValid()){
+	target.frontMostApp().navigationBar().buttons()["注销"].tap();
+}
+target.frontMostApp().mainWindow().tableViews()[0].cells().firstWithPredicate("name beginswith '用户资料'").tap();
+target.frontMostApp().mainWindow().textFields()["Username"].tap();
+target.frontMostApp().keyboard().typeString("1");
+target.frontMostApp().keyboard().typeString("23@\n");
+target.frontMostApp().keyboard().typeString("1");
+target.frontMostApp().keyboard().typeString("23456\n");
+
 target.frontMostApp().tabBar().buttons()["好友"].tap();
 target.frontMostApp().mainWindow().tableViews()[0].cells()[0].tap();
 target.frontMostApp().mainWindow().tableViews()[0].cells()[1].tap();
-UIALogger.logMessage("进入好友听写站");
-target.delay(1);
-target.frontMostApp().mainWindow().tableViews()[0].cells()[2].tap();
-target.delay(1);
+target.delay(2);
+target.frontMostApp().mainWindow().tableViews()[0].cells()[0].tap();
+target.delay(2);
 target.frontMostApp().mainWindow().tableViews()[0].cells().firstWithPredicate("name beginswith '开始考试'").tap();
-UIALogger.logMessage("进行听力考试");
-target.delay(40);
-UIALogger.logMessage("听力考试结束");
-UIALogger.logMessage("显示听写成绩单");
-target.frontMostApp().mainWindow().tableViews()[0].cells().firstWithPredicate("name beginswith '错题回顾'").tap();
-/*
-var okBtn=UIATarget.localTarget().frontMostApp().mainWindow().buttons()["好的"];
-if(okBtn.checkIsValid()===0){
+
+UIALogger.logMessage("开始进行听力考试");
+
+target.delay(60);
+
+UIALogger.logMessage("听力考试结束，显示听写成绩单");
+
+var cell=target.frontMostApp().mainWindow().tableViews()[0].cells().firstWithPredicate("name beginswith '错题回顾:0/'");
+if(cell.checkIsValid()){
+	cell.tap();
+}else{
+	target.frontMostApp().mainWindow().tableViews()[0].cells().firstWithPredicate("name beginswith '错题回顾'").tap();
+	target.delay(2);
 	target.frontMostApp().navigationBar().buttons()["重做"].tap();
+	UIALogger.logMessage("进入错题重做");
+	target.delay(60);
+	UIALogger.logMessage("错题重做结束");
+	UIALogger.logMessage("显示重做听写成绩单");
+	target.delay(5);
 }
-*/
-target.delay(10);
-target.frontMostApp().navigationBar().buttons()["重做"].tap();
-UIALogger.logMessage("进入错题重做");
-target.delay(40);
-UIALogger.logMessage("错题重做结束");
-UIALogger.logMessage("显示重做听写成绩单");
-target.delay(5);
 
 UIALogger.logPass("CASE #968::考试完成后错题重做");
