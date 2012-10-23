5f65fa6d54b1fd0c12df6a1c5cfb9be7d2823ce2
chenzhenuaa <chenzhenuaa@gmail.com>
2012-10-23 14:45:10 +0800

    alter case #965(2)

--- UITest/965(2)_listening-test-give up.js
+++ UITest/965(2)_listening-test-give up.js
@@ -6,12 +6,12 @@ UIALogger.logStart("CASE #965(2)::听力考试")
 var target=UIATarget.localTarget();
 UIAUserCenter.relogin();
 target.frontMostApp().tabBar().buttons()["好友"].tap();
+target.frontMostApp().mainWindow().tableViews()[0].cells()[0].tap();
 target.frontMostApp().mainWindow().tableViews()[0].cells()[1].tap();
-target.frontMostApp().mainWindow().tableViews()[0].cells()[5].tap();
 target.delay(1);
 target.frontMostApp().mainWindow().tableViews()[0].cells()[0].tap();
 target.frontMostApp().mainWindow().tableViews()[0].cells().firstWithPredicate("name beginswith '开始考试'").tap();
-target.delay(60);
+target.delay(5);
 target.frontMostApp().navigationBar().leftButton().tap();
 
 UIALogger.logPass("CASE #965(2)::听力考试");
