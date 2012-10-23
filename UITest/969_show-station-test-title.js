b92d53c6939f51fa95b99ac49f7f872bcc720377
chenzhenuaa <chenzhenuaa@gmail.com>
2012-10-23 15:32:46 +0800

    alter case #969

--- UITest/969_show-station-test-title.js
+++ UITest/969_show-station-test-title.js
@@ -5,7 +5,7 @@ UIALogger.logStart("CASE #969::显示听写站中的试卷");
 var target=UIATarget.localTarget();
 
 var traverseCells = function(target,tableView){
-	for(var i=2/*,len=tableView.cells().length*/;i<7/*len*/;i++){
+	for(var i=1,len=tableView.cells().length;i<len;i++){
 		if(tableView.cells()[i].isVisible()===0)  break;
 		tableView.cells()[i].tap();
 		target.delay(2);
@@ -22,7 +22,7 @@ target.frontMostApp().mainWindow().tableViews()[0].cells()[4].tap();
 UIALogger.logMessage("进入好友听写站");
 target.delay(5);
 */
-target.frontMostApp().mainWindow().tableViews()[0].cells()[1].tap();
+target.frontMostApp().mainWindow().tableViews()[0].cells()[0].tap();
 traverseCells(target,target.frontMostApp().mainWindow().tableViews()[0]);
 
 UIALogger.logPass("CASE #969::显示听写站中的试卷");
