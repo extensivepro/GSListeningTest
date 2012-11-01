24cdcc957df788d3616e4857424ad5d0af481cfb
chenzhenuaa <chenzhenuaa@gmail.com>
2012-11-01 12:04:58 +0800

    alter case #830: add alert

--- UITest/830_open-undownloaded.js
+++ UITest/830_open-undownloaded.js
@@ -2,8 +2,25 @@
 
 UIALogger.logStart("CASE #830::打开未下载完成的听力试卷");
 
-UIAUserCenter.relogin();
 var target=UIATarget.localTarget();
+
+UIATarget.onAlert=function onAlert(alert){
+	var title = alert.name();
+	// test if your script should handle the alert, and if so, return true
+	var info = alert.staticTexts()[0];
+	UIALogger.logDebug(info.name());
+	if(info.name()=="提示"){
+		alert.defaultButton().tap();
+		UIALogger.logDebug("听力试卷未下载完成验证");
+		return true;
+	}
+	// add a warning to the log for each alert encountered
+	UIALogger.logWarning("Alert with title'"+ title +"'encountered!");
+	// otherwise, return false to use the default handler
+	return false;
+}
+
+UIAUserCenter.relogin();
 target.frontMostApp().tabBar().buttons()["好友"].tap();
 target.frontMostApp().mainWindow().tableViews()[0].cells()[0].tap();
 target.frontMostApp().mainWindow().tableViews()[0].cells()[2].tap();
@@ -11,6 +28,8 @@ target.frontMostApp().mainWindow().tableViews()[0].cells()[2].tap();
 target.delay(2);
 
 target.frontMostApp().mainWindow().tableViews()[0].cells()[3].tap();
+
+target.delay(3);
 
 UIALogger.logMessage("打开未下载完成听力试卷通过");
 
